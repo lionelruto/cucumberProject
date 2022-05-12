afbd5e541b94ec5b19d8ddb1a57e730252bd21f4
Lionel Mambingo <lionelmambingo@gmail.com>
2022-05-12 16:14:32 +0100

    Initial commit

---
+++ src/test/java/CucumberTest.java
@@ -0,0 +1,24 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CucumberTest {
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
